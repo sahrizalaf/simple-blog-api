87c771ccced4872ba5147e02626552b503fc04c5
Sahrizal Amirul Firdaus <sahrizal029@gmail.com>
2024-02-12 14:07:21 +0700

    add user validator

--- routes/user.routes.ts
+++ routes/user.routes.ts
@@ -3,8 +3,9 @@ import {
   createUserController,
   getUserByUsernameController,
 } from "../controllers/user.controllers";
+import { validateUser } from "../utils/user.validator";
 
 export const userRoutes = express.Router();
 
-userRoutes.post("/", createUserController);
+userRoutes.post("/", validateUser, createUserController);
 userRoutes.get("/:username", getUserByUsernameController);
---
+++ utils/user.validator.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from "express";
+import Joi, { ValidationError, ValidationResult } from "Joi";
+
+export const userSchema = Joi.object({
+  id: Joi.number().integer().required(),
+  email: Joi.string().email().required(),
+  username: Joi.string().required().min(8),
+  name: Joi.string().required(),
+});
+
+export const validateUser = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const result: ValidationResult = userSchema.validate(req.body, {
+    abortEarly: false,
+  });
+
+  if (result.error) {
+    return res.status(422).json({
+      message: "Invalid request data",
+      errors: result.error.details.map((err) => err.message),
+    });
+  }
+
+  next();
+};
